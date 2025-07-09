b1c6fe133f76279620861047e992ba7e375c989b
zangky <zangky008@gmail.com>
2025-07-09 22:47:37 +0700

    penyesuaian usernae

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'u858331583_grenduta';
-$db['default']['password'] = 'Grendut4';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
 $db['default']['database'] = 'u858331583_grenduta';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
