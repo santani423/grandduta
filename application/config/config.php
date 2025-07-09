6e68e6629435d4ef2723323294ed5e2046ae883c
santani423 <santani423@gmail.com>
2025-07-09 22:33:42 +0700

    penyesuaian config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/granddutanew/';
+$config['base_url']	= 'https://grandduta.undesia.com/';
 $config['per_page'] = 10;
 
 /*
