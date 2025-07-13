fcc5a7be4b8989e1dd1e8680cbeed6c5ecda8351
zangky <zangky008@gmail.com>
2025-07-13 11:52:20 +0700

    perubahan icon pada view

--- application/views/penagihan/transaksi_view.php
+++ application/views/penagihan/transaksi_view.php
@@ -63,11 +63,11 @@
     <div class="panel-footer">   
         <div class="row"> 
             <div class="col-md-10 col-sm-12 col-md-offset-2 col-sm-offset-0">
-                <a href="<?php echo site_url('anggota'); ?>" class="btn btn-default">
-                    <i class="glyphicon glyphicon-chevron-left"></i> Kembali
+                <a href="<?php echo site_url('anggota'); ?>" class="btn btn-warning">
+                    <i class="fa fa-chevron-left"></i> Kembali
                 </a> 
                 <button type="submit" class="btn btn-primary" name="post">
-                    <i class="glyphicon glyphicon-floppy-save"></i> Cari 
+                    <i class="fa fa-search"></i> Cari 
                 </button>                  
             </div>
         </div>
