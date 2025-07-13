9c5378e07922046941518d7fd2795e96a610472a
santani423 <santani423@gmail.com>
2025-07-13 16:15:25 +0700

    menampilkan chart per bulan

--- application/views/home/dashboard.php
+++ application/views/home/dashboard.php
@@ -1,3 +1,50 @@
+<?php
+$this->load->model('Master_model');
+$tahun_ini = date('Y'); // Mendapatkan tahun saat ini, misalnya 2025
+$dataTagihan = $this->Master_model->get_tagihan_per_bulan($tahun_ini);
+
+// Daftar nama bulan
+$namaBulan = [
+    '01' => 'Jan', '02' => 'Feb', '03' => 'Mar', '04' => 'Apr',
+    '05' => 'Mei', '06' => 'Jun', '07' => 'Jul', '08' => 'Agu',
+    '09' => 'Sep', '10' => 'Okt', '11' => 'Nov', '12' => 'Des'
+];
+
+// Siapkan data chart untuk JavaScript
+$chartData = [["Bulan", "Lunas", "Belum Lunas"]];
+foreach ($dataTagihan as $row) {
+    $bulan = str_pad($row['bulan'], 2, '0', STR_PAD_LEFT);
+    $labelBulan = (isset($namaBulan[$bulan]) ? $namaBulan[$bulan] : $bulan) . ' ' . $tahun_ini;
+
+    $chartData[] = [$labelBulan, (int)$row['jumlah_lunas'], (int)$row['jumlah_belum_lunas']];
+}
+?>
+
+<!-- Load Google Chart -->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
+<script type="text/javascript">
+    google.charts.load('current', {'packages': ['bar']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable(<?php echo json_encode($chartData); ?>);
+
+        var options = {
+            chart: {
+                title: 'Statistik Tagihan Tahun <?php echo $tahun_ini; ?>',
+                subtitle: 'Lunas vs Belum Lunas per Bulan',
+            },
+            bars: 'vertical',
+            height: 500,
+            colors: ['#1b9e77', '#d95f02']
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+    }
+</script>
+
 <div class="row">
     <!-- Panel: Data Pelanggan -->
     <div class="col-lg-3 col-md-6 mb-4">
@@ -103,3 +150,6 @@
         </div>
     </div>
 </div>
+
+<!-- Tempat Chart -->
+<div id="columnchart_material" style="width: 100%; height: 500px;"></div>
